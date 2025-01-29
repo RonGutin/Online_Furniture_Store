0f281af1bd0e8f3940bd338694dc497cec584ace
AtiaHili <hili@seenity.com>
2025-01-29 17:40:18 +0200

    inventory class created and endpoint api example

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from app.models.inventory import Inventory
 
 api_blueprint = Blueprint('api', __name__)
 
@@ -13,3 +14,40 @@ def handle_data():
 
 def register_endpoints(app):
     app.register_blueprint(api_blueprint, url_prefix='/api')
+
+
+@api_blueprint.route('/update_quntity', methods=['GET'])
+def update_quntity():
+    try: # get info from url request
+        furniture_type = request.args.get('furniture_type', type=int) 
+        color = request.args.get('color', type=str)
+        high = request.args.get('high', type=int)
+        depth = request.args.get('depth', type=int)
+        width = request.args.get('width', type=int)
+        is_adjustable = request.args.get('is_adjustable', type=bool)
+        has_armrest = request.args.get('has_armrest', type=bool)
+        material = request.args.get('material', type=str)
+        action = request.args.get('action', type=str) # regular user can send only '1'(buying = remove from inventory) (bool)
+        quntity = request.args.get('quntity', type=str)
+        inventory_object = Inventory()
+        furniture_id = inventory_object.get_indx_furniture_by_values(furniture_type=furniture_type, color=color, high=high, depth=depth, width=width, is_adjustable=is_adjustable, has_armrest=has_armrest, material=material, inventory_object=inventory_object)
+        if not furniture_id: 
+            return jsonify({"error: DB connection failed"}), 500
+        if furniture_id == -1:
+            return jsonify({"error: Missing required parameters/there is no furniture with those params"}), 400
+        if furniture_id:
+            try:
+                res = inventory_object.update_quntity(indx=furniture_id, action=action, quntity=quntity)
+                if res:
+                    return jsonify({"updating succsuss"}), 200
+                if not res:
+                    return jsonify({"error: Inventory updating failed"}), 500
+            except Exception as e:
+                return jsonify({"error": f"An error occurred: {e}"}), 500
+    except Exception as e:
+        return jsonify({"error": f"An error occurred: {e}"}), 500
+
+        
+
+
+
--- app/data/DbConnection.py
+++ app/data/DbConnection.py
@@ -2,9 +2,7 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.sql import text
-from sqlalchemy import Column, Integer, String, Float
-
-
+from sqlalchemy import Column, Integer, String, Float, Boolean, CheckConstraint
 
 DATABASE_USERNAME = "root"
 DATABASE_PASSWORD = "HaHa12345!"
@@ -26,9 +24,7 @@ try:
 
     session.close()
 except Exception as e:
-    print(f"Error connecting to DB or applying discount:: {e}")
-
-
+    print(f"Error connecting to DB or applying discount: {e}")
 
 
 class CouponsCodes(Base):
@@ -36,3 +32,39 @@ class CouponsCodes(Base):
     idCouponsCodes = Column(Integer, primary_key=True, index=True)
     CouponValue = Column(String(48), unique=True, nullable=False)
     Discount = Column(Integer, nullable=False)
+
+
+class InventoryDB(Base):
+    __tablename__ = 'Inventory'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    furniture_type = Column(Integer, nullable=False)
+    color = Column(String(50), nullable=False)
+    f_name = Column(String(500), nullable=False)
+    f_desc = Column(String(1000), nullable=False)
+    price = Column(Float, nullable=False)
+    high = Column(Integer, nullable=False)
+    depth = Column(Integer, nullable=False)
+    width = Column(Integer, nullable=False)
+    is_adjustable = Column(Boolean, nullable=False, default=False)
+    has_armrest = Column(Boolean, nullable=False, default=False)
+    material = Column(String(50), nullable=False)
+    quntity = Column(Integer, nullable=False, default=0)
+
+    __table_args__ = (
+        CheckConstraint(
+            'furniture_type IN (1, 2, 3, 4, 5)',
+            name='check_furniture_type'
+        ),
+    )
+
+class UserDB(Base):
+    """SQLAlchemy model for User table"""
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)  # Will store the hashed password from Authentication || need to think if it should be here or not (security)
+    address = Column(String(255), nullable=False)
+    credit = Column(Float, default=0.0)
+    type = Column(String(50))  # For user/manager discrimination
